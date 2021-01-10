50d12c453a92075760bbbff2c3ae3359ac92c532
Anbarasan <anbarasan@thinknsolutions.com>
2021-01-10 20:01:10 +0530

    Initial Commit
    Worked on baisc component setup & create two component to start development.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { InputField } from './Components/InputField/inputField.component';
+import { Card } from './Components/Card/card.component';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(){
+    super()
+    this.state = {
+        todos : ["Testing"]
+    }
+  }
+
+  render(){
+    return (
+      <main className="main-todo-list">
+        <section className="secton-todo-list">
+          <InputField  />
+          <Card todos={this.state.todos} />
+        </section>
+      </main>
+    );
+  }
+  
+
 }
 
 export default App;
---
+++ src/Components/Card/card.component.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Card = (props) => {
+    return(
+        <h1>{props.todos}</h1>
+    )
+}
